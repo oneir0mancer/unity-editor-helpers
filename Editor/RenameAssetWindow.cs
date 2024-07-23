c77cf40a6c823b2129bb8a885718e6eb98b1c8d0
Andrey Sugonyaev <34348146+oneir0mancer@users.noreply.github.com>
2024-07-23 22:27:58 +0300

    Rename subassets tool

---
+++ Editor/RenameAssetWindow.cs
@@ -0,0 +1,41 @@
+﻿﻿using UnityEditor;
+using UnityEngine;
+
+namespace Oneiromancer.EditorHelpers
+{
+    public class RenameAssetWindow : EditorWindow
+    {
+        private Object _asset;
+        private string _newName;
+
+        [MenuItem("Assets/Rename Subasset", false, 19)]
+        [MenuItem("CONTEXT/ScriptableObject/Rename")]
+        private static void OpenWindow()
+        {
+            var editor = GetWindow<RenameAssetWindow>("Rename");
+            if (Selection.activeObject != null && Selection.count == 1)
+            {
+                editor.InjectObject(Selection.activeObject);
+            }
+        }
+
+        private void OnGUI()
+        {
+            _asset = EditorGUILayout.ObjectField("Asset: ", _asset, typeof(Object), false) as Object;
+            _newName = EditorGUILayout.TextField("Rename to: ", _newName);
+            
+            if (GUILayout.Button("Rename"))
+            {
+                _asset.name = _newName;
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+            }
+        }
+
+        private void InjectObject(Object obj)
+        {
+            _asset = obj;
+            _newName = obj.name;
+        }
+    }
+}
